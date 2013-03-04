47a4a0f90bab73206fcb26734a48522aa417ebdd
Ivan Pesin <ivan.pesin@gmail.com>
2013-03-05 00:04:50 +0200

    -clouds intro app

---
+++ hello.rb
@@ -0,0 +1,25 @@
+
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+	sleep 0.1
+ 	erb :index
+end
+
+get '/capacity' do
+	erb :capacity
+end
+
+get '/xaas' do
+	erb :xaas
+end
+
+get '/heroku' do
+	erb :heroku
+end
+
+get '/mu-4e751d19-93618847-70efbdd1-582d6005' do
+	  '42'
+end
+
